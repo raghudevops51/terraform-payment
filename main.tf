294cf439a0a4b7e3fb4e4535fc59e4ca70449b6b
root <root@ip-172-31-34-152.ec2.internal>
2020-12-31 08:40:16 +0000

    ss

---
+++ main.tf
@@ -0,0 +1,11 @@
+module "payment" {
+  source        = "git::https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps51/_git/terraform-backend"
+  bucket        = var.bucket
+  key           = var.key
+  ENV           = var.ENV
+  INSTANCE_TYPE = var.INSTANCE_TYPE
+  KEY_NAME      = var.KEY_NAME
+  component     = "payment"
+  lb_priority   = 500
+}
+
